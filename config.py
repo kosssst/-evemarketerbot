17b4b8a29ce3839c3f9185882ec9ce8f057ed559
kostyamed <kostyamed0003@gmail.com>
2022-03-23 16:39:56 +0200

    added ore

---
+++ config.py
@@ -0,0 +1,7 @@
+marketurl = "https://api.evemarketer.com/ec/marketstat/json"
+key = "evemarketerbot-49efda14a63b.json"
+scope = [
+    'https://www.googleapis.com/auth/drive',
+    'https://www.googleapis.com/auth/drive.file'
+    ]
+file = "json/item_data.json"
--- evemarketer.py
+++ evemarketer.py
@@ -1,29 +1,48 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import requests
-# import json
+import json
+import schedule
+import config
+import time
 
-itemid = 18
-
-marketurl = "https://api.evemarketer.com/ec/marketstat/json"
-
-scope = [
-    'https://www.googleapis.com/auth/drive',
-    'https://www.googleapis.com/auth/drive.file'
-    ]
-file_name = 'evemarketerbot-49efda14a63b.json'
-creds = ServiceAccountCredentials.from_json_keyfile_name(file_name,scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name(config.key,config.scope)
 client = gspread.authorize(creds)
-
-param = dict(typeid=itemid)
-
-resp = requests.get(marketurl, param)
-data = resp.json()
-
-avg_price = data[0]["sell"]["avg"]
-
 sheet = client.open('evemarketer')
 
-ws = sheet.worksheet("Ore")
+print("[INFO]: Connected to Google Sheets API")
 
-ws.update('B2', avg_price)
+def update_prices():
+	try:
+		json_file = open(config.file, 'r')
+	except:
+		print("[ERROR]: Invalid file")
+		return
+
+	try:
+		json_data = json.load(json_file)
+	except:
+		print("[ERROR]: Not json file")
+		return
+
+	for item_type in json_data:
+		ws = sheet.worksheet(json_data[item_type]["sheetname"])
+
+		for item in json_data[item_type]["items"]:
+			itemid = json_data[item_type]["items"][item]["typeid"]
+			cell = json_data[item_type]["items"][item]["cell"]
+			resp = 	requests.get(config.marketurl, dict(typeid=itemid))
+			print("[INFO]: Sended request by typeid " + str(itemid))
+			data = resp.json()
+			avg_price = data[0]["sell"]["avg"]
+			ws.update(cell, avg_price)
+			print("[INFO]: Updated cell " + cell + " by value " + str(avg_price))
+
+
+
+
+schedule.every(1).minutes.do(update_prices)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)

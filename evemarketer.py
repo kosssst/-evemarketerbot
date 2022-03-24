ab80e19f29af68bbe4a3d210719b359cc859760b
kostyamed <kostyamed0003@gmail.com>
2022-03-24 11:48:54 +0200

    Fixed bug with exit

--- evemarketer.py
+++ evemarketer.py
@@ -38,55 +38,56 @@ try:
 				try:
 					resp = 	requests.get(config.marketurl, dict(typeid=itemid))
 					# print("[INFO]: Sended request by typeid " + str(itemid) + " (" + str(name) + ")")
-					data = resp.json()
-					buy_volume = data[0]["buy"]["volume"]
-					ws.update(("B" + line), buy_volume)
-					buy_wavg = data[0]["buy"]["wavg"]
-					ws.update(("C" + line), buy_wavg)
-					buy_avg = data[0]["buy"]["avg"]
-					ws.update(("D" + line), buy_avg)
-					buy_min = data[0]["buy"]["min"]
-					ws.update(("E" + line), buy_min)
-					buy_max = data[0]["buy"]["max"]
-					ws.update(("F" + line), buy_max)
-					buy_variance = data[0]["buy"]["variance"]
-					ws.update(("G" + line), buy_variance)
-					buy_stddev = data[0]["buy"]["stdDev"]
-					ws.update(("H" + line), buy_stddev)
-					buy_median = data[0]["buy"]["median"]
-					ws.update(("I" + line), buy_median)
-					buy_five = data[0]["buy"]["fivePercent"]
-					ws.update(("J" + line), buy_five)
-
-					sell_volume = data[0]["sell"]["volume"]
-					ws.update(("K" + line), sell_volume)
-					sell_wavg = data[0]["sell"]["wavg"]
-					ws.update(("L" + line), sell_wavg)
-					sell_avg = data[0]["sell"]["avg"]
-					ws.update(("M" + line), sell_avg)
-					sell_min = data[0]["sell"]["min"]
-					ws.update(("N" + line), sell_min)
-					sell_max = data[0]["sell"]["max"]
-					ws.update(("O" + line), sell_max)
-					sell_variance = data[0]["sell"]["variance"]
-					ws.update(("P" + line), sell_variance)
-					sell_stddev = data[0]["sell"]["stdDev"]
-					ws.update(("Q" + line), sell_stddev)
-					sell_median = data[0]["sell"]["median"]
-					ws.update(("R" + line), sell_median)
-					sell_five = data[0]["sell"]["fivePercent"]
-					ws.update(("S" + line), sell_five)
-					
-					now = datetime.now()
-					current_time = now.strftime("%H:%M:%S")
-					print("[" + str(current_time) + "] [INFO]: Updated item " + str(name))
-
-					time.sleep(10)
 				except:
 					now = datetime.now()
 					current_time = now.strftime("%H:%M:%S")
 					print("[" + str(current_time) + "] [ERROR]: Skipped " + str(name))
 					continue
+				data = resp.json()
+				buy_volume = data[0]["buy"]["volume"]
+				ws.update(("B" + line), buy_volume)
+				buy_wavg = data[0]["buy"]["wavg"]
+				ws.update(("C" + line), buy_wavg)
+				buy_avg = data[0]["buy"]["avg"]
+				ws.update(("D" + line), buy_avg)
+				buy_min = data[0]["buy"]["min"]
+				ws.update(("E" + line), buy_min)
+				buy_max = data[0]["buy"]["max"]
+				ws.update(("F" + line), buy_max)
+				buy_variance = data[0]["buy"]["variance"]
+				ws.update(("G" + line), buy_variance)
+				buy_stddev = data[0]["buy"]["stdDev"]
+				ws.update(("H" + line), buy_stddev)
+				buy_median = data[0]["buy"]["median"]
+				ws.update(("I" + line), buy_median)
+				buy_five = data[0]["buy"]["fivePercent"]
+				ws.update(("J" + line), buy_five)
+
+				sell_volume = data[0]["sell"]["volume"]
+				ws.update(("K" + line), sell_volume)
+				sell_wavg = data[0]["sell"]["wavg"]
+				ws.update(("L" + line), sell_wavg)
+				sell_avg = data[0]["sell"]["avg"]
+				ws.update(("M" + line), sell_avg)
+				sell_min = data[0]["sell"]["min"]
+				ws.update(("N" + line), sell_min)
+				sell_max = data[0]["sell"]["max"]
+				ws.update(("O" + line), sell_max)
+				sell_variance = data[0]["sell"]["variance"]
+				ws.update(("P" + line), sell_variance)
+				sell_stddev = data[0]["sell"]["stdDev"]
+				ws.update(("Q" + line), sell_stddev)
+				sell_median = data[0]["sell"]["median"]
+				ws.update(("R" + line), sell_median)
+				sell_five = data[0]["sell"]["fivePercent"]
+				ws.update(("S" + line), sell_five)
+				
+				now = datetime.now()
+				current_time = now.strftime("%H:%M:%S")
+				print("[" + str(current_time) + "] [INFO]: Updated item " + str(name))
+
+				time.sleep(10)
+				
 					
 except KeyboardInterrupt:
 	pass
